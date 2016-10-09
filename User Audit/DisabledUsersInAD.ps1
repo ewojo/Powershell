5aebc15443e44d954906137ab166fa5a2d3e35fc
voytovitz <voytovitz@users.noreply.github.com>
2016-10-08 21:12:15 -0500

    added AD Audit

---
+++ User Audit/DisabledUsersInAD.ps1
@@ -0,0 +1,13 @@
+﻿###############################################
+#
+#     Find Disabled Users In OU
+#     Made By Eric Wojtowicz
+#     Date 3/24/2016
+#      
+#
+###############################################
+import-module activedirectory
+Get-ADUser -Filter {Enabled -eq $false} -SearchBase “OU=DevOps,DC=Contoso,DC=com” -Properties * | 
+    select -Property Name,DisplayName,mail,SamAccountName, Enabled |
+    Sort-Object -Property SamAccountName | 
+    Export-csv "C:\TEST\DisabledUsers.csv" 
