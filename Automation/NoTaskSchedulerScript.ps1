f7067184d675a092cf50f7bad9cc32039490059d
Eric Wojtowicz <ewojo@users.noreply.github.com>
2017-01-10 09:42:39 -0600

    Scheduled Powershell Script
    Automated Running Powershell Script without using task scheduler.

---
+++ Automation/NoTaskSchedulerScript.ps1
@@ -0,0 +1,8 @@
+#Run Powershell Script without task scheduler
+while ($true)
+{
+     #Insert Powershell script you want to run
+     Write-Host 'hello world';
+     #Put (60* minutes)
+     sleep -seconds (60)
+}
