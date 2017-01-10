ff8947e7b08942dfa2d6fb6949f91d1a93ec5186
Eric Wojtowicz <ewojo@users.noreply.github.com>
2017-01-10 09:45:29 -0600

    Update NoTaskSchedulerScript.ps1

--- Automation/NoTaskSchedulerScript.ps1
+++ Automation/NoTaskSchedulerScript.ps1
@@ -2,7 +2,7 @@
 while ($true)
 {
      #Insert Powershell script you want to run
-     Write-Host 'hello world';
+     Write-Host 'Script Ran @' (Get-Date);
      #Put (60* minutes)
      sleep -seconds (60)
 }
